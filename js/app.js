3060943a5d32310078916caf614a1aabb1c15c43
kotatsune <kotatsune@gmail.com>
2014-03-02 21:14:31 +0900

    Box 表示に向けての準備
    頂点バッファ、インデックス バッファの作成ができるようになった。

--- js/app.js
+++ js/app.js
@@ -23,10 +23,113 @@ function App() {}
 //--------------------------------------------------------------------------------
 // 定数
 //--------------------------------------------------------------------------------
-App.prototype.CANVAS_ID = 'screen';
-App.prototype.CANVAS_WIDTH = 640;
-App.prototype.CANVAS_HEIGHT = 480;
-App.prototype.FPS = 1;
+App.CANVAS_ID = 'screen';
+App.CANVAS_WIDTH = 640;
+App.CANVAS_HEIGHT = 480;
+App.FPS = 1;
+App.SCRIPT_TYPE_VERTEX_SHADER = "x-shader/x-vertex";
+App.SCRIPT_TYPE_FRAGMENT_SHADER = "x-shader/x-fragment";
+
+
+//--------------------------------------------------------------------------------
+// シェーダーを作成します。
+//--------------------------------------------------------------------------------
+App.prototype.createShader = function ( gl, id )
+{
+	var script = document.getElementById( id );
+	if ( !script )
+	{
+		alert( 'Script not found.' );
+
+		return null;
+	}
+	
+	var shader = null;
+	switch ( script.type )
+	{
+		case App.SCRIPT_TYPE_VERTEX_SHADER :
+			{
+				shader = gl.createShader( gl.VERTEX_SHADER );
+			}
+			break;
+			
+		case App.SCRIPT_TYPE_FRAGMENT_SHADER :
+			{
+				shader = gl.createShader( gl.FRAGMENT_SHADER );
+			}
+			break;
+
+		default :
+			{
+				alert( 'Bad script type.' );
+			}
+			return null;
+	}
+	
+	gl.shaderSource( shader, script.text );
+	gl.compileShader( shader ); 
+	if ( !gl.getShaderParameter( shader, gl.COMPILE_STATUS ) )
+	{
+		alert( gl.getShaderInfoLog( shader ) );
+
+		return null;
+	}
+
+	return shader;
+};
+
+
+//--------------------------------------------------------------------------------
+// プログラム オブジェクトを作成します。
+//--------------------------------------------------------------------------------
+App.prototype.createProgramObject = function ( gl, vs, fs )
+{
+	var program = gl.createProgram();
+	gl.attachShader( program, vs );
+	gl.attachShader( program, fs );
+	gl.linkProgram( program );
+	
+	if ( !gl.getProgramParameter( program, gl.LINK_STATUS ) )
+	{
+		alert( gl.getProgramInfoLog( program ) );
+
+		return null;
+	}
+
+	gl.useProgram( program );
+
+	return program;
+};
+
+
+//--------------------------------------------------------------------------------
+// VBO を作成します。
+//--------------------------------------------------------------------------------
+App.prototype.createVBO = function ( gl, data )
+{
+	// gl.ARRAY_BUFFER と gl.ELEMENT_ARRAY_BUFFER の違いに注意!!
+	var vbo = gl.createBuffer();
+	gl.bindBuffer( gl.ARRAY_BUFFER, vbo );
+	gl.bufferData( gl.ARRAY_BUFFER, new Float32Array( data ), gl.STATIC_DRAW );
+	gl.bindBuffer( gl.ARRAY_BUFFER, null );
+	
+	return vbo;
+};
+
+
+//--------------------------------------------------------------------------------
+// IBO を作成します。
+//--------------------------------------------------------------------------------
+App.prototype.createIBO = function ( gl, data )
+{
+	// gl.ARRAY_BUFFER と gl.ELEMENT_ARRAY_BUFFER の違いに注意!!
+	var ibo = gl.createBuffer();
+	gl.bindBuffer( gl.ELEMENT_ARRAY_BUFFER, ibo );
+	gl.bufferData( gl.ELEMENT_ARRAY_BUFFER, new Int16Array( data ), gl.STATIC_DRAW );
+	gl.bindBuffer( gl.ELEMENT_ARRAY_BUFFER, null );
+	
+	return ibo;
+};
 
 
 //--------------------------------------------------------------------------------
@@ -34,7 +137,12 @@ App.prototype.FPS = 1;
 //--------------------------------------------------------------------------------
 App.prototype.setupGl = function ( gl )
 {
-}
+	this.vertexShader1 = this.createShader( gl, 'vertex-shader-1' );
+	this.fragmentShader1 = this.createShader( gl, 'fragment-shader-1' );
+	this.programObject1 = this.createProgramObject( gl, this.vertexShader1, this.fragmentShader1 );
+	this.vertices1 = this.createVBO( gl, Model.Box.VERTICES );
+	this.indices1 = this.createIBO( gl, Model.Box.INDICES );
+};
 
 
 //--------------------------------------------------------------------------------
@@ -42,7 +150,8 @@ App.prototype.setupGl = function ( gl )
 //--------------------------------------------------------------------------------
 App.prototype.tearDownGl = function ( gl )
 {
-}
+	// Nothing to do.
+};
 
 
 //--------------------------------------------------------------------------------
@@ -59,8 +168,8 @@ App.prototype.loop = function ( gl )
 	gl.flush();
 	
 	var that = this;
-	setTimeout( function () { that.loop( gl ); }, 1000 / this.FPS );
-}
+	setTimeout( function () { that.loop( gl ); }, 1000 / App.FPS );
+};
 
 
 //--------------------------------------------------------------------------------
@@ -68,14 +177,22 @@ App.prototype.loop = function ( gl )
 //--------------------------------------------------------------------------------
 App.prototype.run = function ()
 {
-	var canvas = document.getElementById( this.CANVAS_ID );
-	if ( canvas )
+	var canvas = document.getElementById( App.CANVAS_ID );
+	if ( !canvas )
 	{
-		var gl = canvas.getContext( 'webgl' ) || canvas.getContext( 'experimental-webgl' );
-		if ( gl )
-		{
-			
-			this.loop( gl );
-		}
+		alert( 'Cannot get canvas.' );
+
+		return;
 	}
-}
+
+	var gl = canvas.getContext( 'webgl' ) || canvas.getContext( 'experimental-webgl' );
+	if ( !gl )
+	{
+		alert( 'Cannot get gl.' );
+
+		return
+	}
+
+	this.setupGl( gl );	
+	this.loop( gl );
+};
